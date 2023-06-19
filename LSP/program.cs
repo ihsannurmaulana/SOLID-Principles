243a1e2e9145c399333e9aa57949dcd904c60539
Shandika Galih <shandika2001@gmail.com>
2023-06-19 22:13:35 +0700

    + LSP

---
+++ LSP/program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+        Motorcycle motorcycle = new Motorcycle();
+
+        StartAndAccelerateVehicle(vehicle);
+        StartAndAccelerateVehicle(car);
+        StartAndAccelerateVehicle(motorcycle);
+
+        Console.ReadLine();
+    }
+
+    public static void StartAndAccelerateVehicle(Vehicle vehicle)
+    {
+        vehicle.StartEngine();
+        vehicle.Accelerate();
+    }
+}
+
